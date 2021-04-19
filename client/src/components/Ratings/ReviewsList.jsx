59950b55050a13a226625b1092dda8ce1009b7c6
Samuel Gasser <sgasser94@gmail.com>
2021-04-19 12:19:02 -0700

    Created click handlers for the More Reviews & Add A Review buttons.

--- client/src/components/Ratings/ReviewsList.jsx
+++ client/src/components/Ratings/ReviewsList.jsx
@@ -8,15 +8,19 @@ class ReviewsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // tiles: 2,
+      renderCreate: false,
     };
+
+    this.openCreateReviewModal = this.openCreateReviewModal.bind(this);
   }
 
   componentDidMount() {
-    console.log(this.props.productNum);
+    // need to rework this request for pagination
+    // var url1 = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews?product_id=${this.props.productNum}`;
+    const url2 = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews?product_id=23146';
     $.ajax({
       method: 'GET',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews?product_id=${this.props.productNum}`,
+      url: url2,
       headers: {
         Authorization: config.API_KEY,
       },
@@ -30,16 +34,31 @@ class ReviewsList extends React.Component {
     });
   }
 
+  fetchMoreReviews() {
+    console.log('fetchMoreReviews');
+  }
+
+  openCreateReviewModal() {
+    console.log('toggling create modal');
+    this.setState({
+      renderCreate: !this.state.renderCreate,
+    });
+  }
+
   render() {
     const reviews = this.state.list || [];
+    const createReviewElement = this.state.renderCreate ? <CreateReview /> : '';
     return (
       <div id="tiles">
-        <p>{reviews.length} reviews, sorted by relevance</p>
-        {reviews.map(item => {
-          return <ReviewTile key={item.review_id} review={item}/>
-        })}
-        <p>Hardcoded CreateReview</p>
-        <CreateReview />
+        <p>
+          {reviews.length}
+          {' '}
+          reviews, sorted by relevance
+        </p>
+        {reviews.map((item) => <ReviewTile key={item.review_id} review={item} />)}
+        <button type="button" onClick={this.fetchMoreReviews}>MORE REVIEWS</button>
+        <button type="button" onClick={this.openCreateReviewModal}>ADD A REVIEW</button>
+        {createReviewElement}
       </div>
     );
   }
--- client/src/components/Ratings/children/ReviewTile.jsx
+++ client/src/components/Ratings/children/ReviewTile.jsx
@@ -9,16 +9,21 @@ class ReviewTile extends React.Component {
   }
 
   render() {
-    console.log('from tile', this.props.review);
     var recommendation = this.props.review.recommend ? 'I recommend this product' : '';
+    var response = this.props.review.response ? this.props.review.response : '';
     return (
       <div id="review-tile">
         <p>{this.props.review.rating} Stars</p>
         <strong>{this.props.review.summary}</strong>
         <p>{recommendation}</p>
         <p>{this.props.review.body}</p>
-        <p>Sales team response</p>
-        <button type="button">Helpful?</button>
+        <p>{response}</p>
+        <p>
+          Helpful?
+          <button type="button">Yes</button>
+          ({this.props.review.helpfulness})
+          <button type="button">No</button>
+        </p>
       </div>
     );
   }

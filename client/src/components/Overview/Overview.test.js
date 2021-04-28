bd3ed79204f2e3ab68e0ebe8b8d807ff6d8e806b
Will Trinh <williamltrinh@gmail.com>
2021-04-28 11:25:43 -0700

    installed tests dependencies

--- client/src/components/Overview/Overview.test.js
+++ client/src/components/Overview/Overview.test.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-undef */
 // import dependencies
 import React from 'react';
-
 // import react-testing methods
-import {
-  render, fireEvent, waitFor, screen,
-} from '@testing-library/react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 // Resolve react-slick matchMedia error
 import '../../matchMedia';
 // the component to test
 import Overview from './Overview';
 
-test('adds 2 + 2 to equal 4', () => {
-  expect(2 + 2).toBe(4);
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+test('Overview render', () => {
+  const component = renderer.create(
+    <Overview />,
+  );
 });

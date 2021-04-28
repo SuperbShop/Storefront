136fec570ed742e262f830ba687422c9e46fbc83
Will Trinh <williamltrinh@gmail.com>
2021-04-28 11:08:55 -0700

    Basic tests

--- client/src/components/Overview/Overview.test.js
+++ client/src/components/Overview/Overview.test.js
@@ -1,5 +1,15 @@
+// import dependencies
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+
+// import react-testing methods
+import {
+  render, fireEvent, waitFor, screen,
+} from '@testing-library/react';
+// Resolve react-slick matchMedia error
 import '../../matchMedia';
+// the component to test
 import Overview from './Overview';
 
+test('adds 2 + 2 to equal 4', () => {
+    expect(2 + 2).toBe(4);
+  });
